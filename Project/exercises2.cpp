a0fcd6a219c495fc22ab7a6af45f3935aa40feac
noya428 <noya7740@outlook.com>
2026-08-24 10:45:04 +0900

    Exercises0824

---
+++ Project/exercises2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[5] = { 10, 20, 30, 40, 50 };
+
+    int* p = numbers;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << *p << endl;
+
+        p++;
+    }
+
+    return 0;
+}
